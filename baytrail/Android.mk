963d1592f8f954e51a575fcde1d0dfae7a759c0a
Olga Kasatkina <olga.a.kasatkina@intel.com>
2015-09-30 21:08:27 +0300

    ww40.2 MediaSDK drop for Android M
    MediaSDK product version: 6.0.013444
    MediaSDK file version: 6.15.9.29
    
    List of changes for MediaSDK library:
    - Added workaround for OAM-3409 and OAM-2279
    
    Commits:
    - MediaSDK library: Workaround for OAM-3409, OAM-2279
      (I147393a0c7827411309a425a5bd34b80356eb3c7)
    - MediaSDK samples OpenMAX plug-ins:
      Dummy support of OMX_VIDEO_PARAM_ANDROID_VP8ENCODERTYPE in decoders
      (I724826ee5ade9b3f9e6db88877396c5c6d2636e7)
    - UFO GEN7: VPG GEN7 x86/x86_64 UFO prebuilts - ww37.5
      (Ic4422051a228ad09c9a44d85e31ca8bd372a1a7c)
    - UFO GEN8: VPG GEN8 x86/x86_64 UFO prebuilts for M - ww37.2
      (Id42288b3b4e1b1bec2edd4f1ac60cdcf0edf5e90)
    - UFO GEN9: VPG: Adding DMC binary ver 1.04 for BXT
      (I235e59b0a56d8a5bc683fafc5780961d0101827b)
    - LibVA: Merge remote-tracking branch
      'origin/abt/topic/gmin/l-dev/graphics-gen/master'
      into gmin/l-dev/master (I1a5126d81a48a719b4e3ba2b56daaa1cc0b0b843)
    
    Change-Id: I459b228fc4eb457d35a1560360384c7be2083c98
    Tracked-On: OAM-2279
    Signed-off-by: Olga Kasatkina <olga.a.kasatkina@intel.com>
    Reviewed-on: 418470

--- baytrail/Android.mk
+++ baytrail/Android.mk
@@ -2,149 +2,154 @@ LOCAL_PATH := $(call my-dir)
 
 ifneq ($(filter $(TARGET_BOARD_PLATFORM),baytrail gmin coho cohol),)
 
-  # Include files
-  include $(CLEAR_VARS)
-
-  LOCAL_COPY_HEADERS_TO := mediasdk
-
-  LOCAL_COPY_HEADERS := \
-    include/mfxastructures.h \
-    include/mfxaudio++.h \
-    include/mfxaudio.h \
-    include/mfxcamera.h \
-    include/mfxcommon.h \
-    include/mfxdefs.h \
-    include/mfxenc.h \
-    include/mfxfei.h \
-    include/mfxhdcp.h \
-    include/mfxjpeg.h \
-    include/mfxla.h \
-    include/mfxmvc.h \
-    include/mfxpcp.h \
-    include/mfxplugin++.h \
-    include/mfxplugin.h \
-    include/mfxplugin_internal.h \
-    include/mfxsession.h \
-    include/mfxstructures.h \
-    include/mfxvideo++.h \
-    include/mfxvideo.h \
-    include/mfxvp8.h \
-    include/mfxvp9.h \
-    include/mfxvstructures.h \
-    include/mfxwidi.h
-
-  include $(BUILD_COPY_HEADERS)
-
-  # Documents
-  include $(CLEAR_VARS)
-
-  LOCAL_MODULE := mediasdk_release_notes.pdf
-  LOCAL_MODULE_OWNER := intel
-  LOCAL_SRC_FILES := doc/$(LOCAL_MODULE)
-  LOCAL_MODULE_TAGS := optional
-  LOCAL_MODULE_CLASS := ETC
-
-  include $(BUILD_PREBUILT)
-
-  # If Media SDK sources repo presents - no need to execute below block
-  ifneq ($(BOARD_HAVE_MEDIASDK_SRC),true)
-    # Shared libraries
+    # Include files
     include $(CLEAR_VARS)
-    LOCAL_MODULE := mfx_prebuilts
-    LOCAL_MODULE_TAGS := optional
+
+    LOCAL_COPY_HEADERS_TO := mediasdk
+
+    LOCAL_COPY_HEADERS := \
+        include/mfxastructures.h \
+        include/mfxaudio++.h \
+        include/mfxaudio.h \
+        include/mfxcamera.h \
+        include/mfxcommon.h \
+        include/mfxdefs.h \
+        include/mfxenc.h \
+        include/mfxfei.h \
+        include/mfxhdcp.h \
+        include/mfxjpeg.h \
+        include/mfxla.h \
+        include/mfxmvc.h \
+        include/mfxpcp.h \
+        include/mfxplugin++.h \
+        include/mfxplugin.h \
+        include/mfxplugin_internal.h \
+        include/mfxsession.h \
+        include/mfxstructures.h \
+        include/mfxvideo++.h \
+        include/mfxvideo.h \
+        include/mfxvp8.h \
+        include/mfxvp9.h \
+        include/mfxvstructures.h \
+        include/mfxwidi.h
+
+    include $(BUILD_COPY_HEADERS)
+
+    # Documents
+    include $(CLEAR_VARS)
+
+    LOCAL_MODULE := mediasdk_release_notes.pdf
     LOCAL_MODULE_OWNER := intel
-    LOCAL_REQUIRED_MODULES :=
-    LOCAL_REQUIRED_MODULES += libmfx_omx_core
-    LOCAL_REQUIRED_MODULES += libmfx_omx_components_hw
-    LOCAL_REQUIRED_MODULES += libmfxhw32
-    LOCAL_REQUIRED_MODULES += libmfxsw32
-    LOCAL_REQUIRED_MODULES += libmfx_omx_components_sw
-    ifeq ($(TARGET_ARCH), x86_64)
-        LOCAL_REQUIRED_MODULES += libmfxhw64
+    LOCAL_SRC_FILES := doc/$(LOCAL_MODULE)
+    LOCAL_MODULE_TAGS := optional
+    LOCAL_MODULE_CLASS := ETC
+
+    include $(BUILD_PREBUILT)
+
+    # If Media SDK sources repo presents - no need to execute below block
+    ifneq ($(BOARD_HAVE_MEDIASDK_SRC),true)
+        # Shared libraries
+        include $(CLEAR_VARS)
+        LOCAL_MODULE := mfx_prebuilts
+        LOCAL_MODULE_TAGS := optional
+        LOCAL_MODULE_OWNER := intel
+        LOCAL_REQUIRED_MODULES :=
+        ifneq ($(BOARD_HAVE_OMX_SRC),true)
+            LOCAL_REQUIRED_MODULES += libmfx_omx_core
+            LOCAL_REQUIRED_MODULES += libmfx_omx_components_hw
+            LOCAL_REQUIRED_MODULES += libmfx_omx_components_sw
+        endif
+        LOCAL_REQUIRED_MODULES += libmfxhw32
+        LOCAL_REQUIRED_MODULES += libmfxsw32
+        ifeq ($(TARGET_ARCH), x86_64)
+            LOCAL_REQUIRED_MODULES += libmfxhw64
+        endif
+        include $(BUILD_PHONY_PACKAGE)
+
+        include $(CLEAR_VARS)
+        LOCAL_MODULE := libmfxhw32
+        LOCAL_MODULE_CLASS := SHARED_LIBRARIES
+        LOCAL_MODULE_TAGS := optional
+        LOCAL_MODULE_STEM := libmfxhw32
+        LOCAL_MODULE_SUFFIX := $(TARGET_SHLIB_SUFFIX)
+        LOCAL_MODULE_OWNER := intel
+        LOCAL_MULTILIB := 32
+        LOCAL_SRC_FILES_32 := lib/x86/libmfxhw32$(LOCAL_MODULE_SUFFIX)
+        include $(BUILD_PREBUILT)
+
+        include $(CLEAR_VARS)
+        LOCAL_MODULE := libmfxsw32
+        LOCAL_MODULE_CLASS := SHARED_LIBRARIES
+        LOCAL_MODULE_TAGS := optional
+        LOCAL_MODULE_STEM := libmfxsw32
+        LOCAL_MODULE_SUFFIX := $(TARGET_SHLIB_SUFFIX)
+        LOCAL_MODULE_OWNER := intel
+        LOCAL_MULTILIB := 32
+        LOCAL_SRC_FILES_32 := lib/x86/libmfxsw32$(LOCAL_MODULE_SUFFIX)
+        include $(BUILD_PREBUILT)
+
+        include $(CLEAR_VARS)
+        LOCAL_MODULE := libmfxhw64
+        LOCAL_MODULE_CLASS := SHARED_LIBRARIES
+        LOCAL_MODULE_TAGS := optional
+        LOCAL_MODULE_STEM := libmfxhw64
+        LOCAL_MODULE_SUFFIX := $(TARGET_SHLIB_SUFFIX)
+        LOCAL_MODULE_OWNER := intel
+        LOCAL_MULTILIB := 64
+        LOCAL_SRC_FILES_64 := lib/x86_64/libmfxhw64$(LOCAL_MODULE_SUFFIX)
+        include $(BUILD_PREBUILT)
+
+        ifneq ($(BOARD_HAVE_OMX_SRC),true)
+            include $(CLEAR_VARS)
+            LOCAL_MODULE := libmfx_omx_core
+            LOCAL_MODULE_CLASS := SHARED_LIBRARIES
+            LOCAL_MODULE_TAGS := optional
+            LOCAL_MODULE_STEM := libmfx_omx_core
+            LOCAL_MODULE_SUFFIX := $(TARGET_SHLIB_SUFFIX)
+            LOCAL_MODULE_OWNER := intel
+            LOCAL_MULTILIB := both
+            LOCAL_SRC_FILES_32 := lib/x86/libmfx_omx_core$(LOCAL_MODULE_SUFFIX)
+            LOCAL_SRC_FILES_64 := lib/x86_64/libmfx_omx_core$(LOCAL_MODULE_SUFFIX)
+            include $(BUILD_PREBUILT)
+
+            include $(CLEAR_VARS)
+            LOCAL_MODULE := libmfx_omx_components_hw
+            LOCAL_MODULE_CLASS := SHARED_LIBRARIES
+            LOCAL_MODULE_TAGS := optional
+            LOCAL_MODULE_STEM := libmfx_omx_components_hw
+            LOCAL_MODULE_SUFFIX := $(TARGET_SHLIB_SUFFIX)
+            LOCAL_MODULE_OWNER := intel
+            LOCAL_MULTILIB := both
+            LOCAL_SRC_FILES_32 := lib/x86/libmfx_omx_components_hw$(LOCAL_MODULE_SUFFIX)
+            LOCAL_SRC_FILES_64 := lib/x86_64/libmfx_omx_components_hw$(LOCAL_MODULE_SUFFIX)
+            include $(BUILD_PREBUILT)
+
+            include $(CLEAR_VARS)
+            LOCAL_MODULE := libmfx_omx_components_sw
+            LOCAL_MODULE_CLASS := SHARED_LIBRARIES
+            LOCAL_MODULE_TAGS := optional
+            LOCAL_MODULE_STEM := libmfx_omx_components_sw
+            LOCAL_MODULE_SUFFIX := $(TARGET_SHLIB_SUFFIX)
+            LOCAL_MODULE_OWNER := intel
+            LOCAL_MULTILIB := 32
+            LOCAL_SRC_FILES_32 := lib/x86/libmfx_omx_components_sw$(LOCAL_MODULE_SUFFIX)
+            include $(BUILD_PREBUILT)
+        endif
+
+        # Executables
+        ifeq ($(MEDIASDK_TEST_APPS),true)
+            include $(CLEAR_VARS)
+
+            LOCAL_PREBUILT_EXECUTABLES := \
+                bin/$(TARGET_ARCH)/sample_decode \
+                bin/$(TARGET_ARCH)/sample_encode \
+                bin/$(TARGET_ARCH)/mfx_player \
+                bin/$(TARGET_ARCH)/mfx_transcoder
+
+          LOCAL_MODULE_TAGS := optional
+          include $(BUILD_MULTI_PREBUILT)
+        endif
+
     endif
-    include $(BUILD_PHONY_PACKAGE)
-
-    include $(CLEAR_VARS)
-    LOCAL_MODULE := libmfxhw32
-    LOCAL_MODULE_CLASS := SHARED_LIBRARIES
-    LOCAL_MODULE_TAGS := optional
-    LOCAL_MODULE_STEM := libmfxhw32
-    LOCAL_MODULE_SUFFIX := $(TARGET_SHLIB_SUFFIX)
-    LOCAL_MODULE_OWNER := intel
-    LOCAL_MULTILIB := 32
-    LOCAL_SRC_FILES_32 := lib/x86/libmfxhw32$(LOCAL_MODULE_SUFFIX)
-    include $(BUILD_PREBUILT)
-
-    include $(CLEAR_VARS)
-    LOCAL_MODULE := libmfxsw32
-    LOCAL_MODULE_CLASS := SHARED_LIBRARIES
-    LOCAL_MODULE_TAGS := optional
-    LOCAL_MODULE_STEM := libmfxsw32
-    LOCAL_MODULE_SUFFIX := $(TARGET_SHLIB_SUFFIX)
-    LOCAL_MODULE_OWNER := intel
-    LOCAL_MULTILIB := 32
-    LOCAL_SRC_FILES_32 := lib/x86/libmfxsw32$(LOCAL_MODULE_SUFFIX)
-    include $(BUILD_PREBUILT)
-
-    include $(CLEAR_VARS)
-    LOCAL_MODULE := libmfxhw64
-    LOCAL_MODULE_CLASS := SHARED_LIBRARIES
-    LOCAL_MODULE_TAGS := optional
-    LOCAL_MODULE_STEM := libmfxhw64
-    LOCAL_MODULE_SUFFIX := $(TARGET_SHLIB_SUFFIX)
-    LOCAL_MODULE_OWNER := intel
-    LOCAL_MULTILIB := 64
-    LOCAL_SRC_FILES_64 := lib/x86_64/libmfxhw64$(LOCAL_MODULE_SUFFIX)
-    include $(BUILD_PREBUILT)
-
-    include $(CLEAR_VARS)
-    LOCAL_MODULE := libmfx_omx_core
-    LOCAL_MODULE_CLASS := SHARED_LIBRARIES
-    LOCAL_MODULE_TAGS := optional
-    LOCAL_MODULE_STEM := libmfx_omx_core
-    LOCAL_MODULE_SUFFIX := $(TARGET_SHLIB_SUFFIX)
-    LOCAL_MODULE_OWNER := intel
-    LOCAL_MULTILIB := both
-    LOCAL_SRC_FILES_32 := lib/x86/libmfx_omx_core$(LOCAL_MODULE_SUFFIX)
-    LOCAL_SRC_FILES_64 := lib/x86_64/libmfx_omx_core$(LOCAL_MODULE_SUFFIX)
-    include $(BUILD_PREBUILT)
-
-    include $(CLEAR_VARS)
-    LOCAL_MODULE := libmfx_omx_components_hw
-    LOCAL_MODULE_CLASS := SHARED_LIBRARIES
-    LOCAL_MODULE_TAGS := optional
-    LOCAL_MODULE_STEM := libmfx_omx_components_hw
-    LOCAL_MODULE_SUFFIX := $(TARGET_SHLIB_SUFFIX)
-    LOCAL_MODULE_OWNER := intel
-    LOCAL_MULTILIB := both
-    LOCAL_SRC_FILES_32 := lib/x86/libmfx_omx_components_hw$(LOCAL_MODULE_SUFFIX)
-    LOCAL_SRC_FILES_64 := lib/x86_64/libmfx_omx_components_hw$(LOCAL_MODULE_SUFFIX)
-    include $(BUILD_PREBUILT)
-
-    include $(CLEAR_VARS)
-    LOCAL_MODULE := libmfx_omx_components_sw
-    LOCAL_MODULE_CLASS := SHARED_LIBRARIES
-    LOCAL_MODULE_TAGS := optional
-    LOCAL_MODULE_STEM := libmfx_omx_components_sw
-    LOCAL_MODULE_SUFFIX := $(TARGET_SHLIB_SUFFIX)
-    LOCAL_MODULE_OWNER := intel
-    LOCAL_MULTILIB := 32
-    LOCAL_SRC_FILES_32 := lib/x86/libmfx_omx_components_sw$(LOCAL_MODULE_SUFFIX)
-    include $(BUILD_PREBUILT)
-
-    # Executables
-    ifeq ($(MEDIASDK_TEST_APPS),true)
-      include $(CLEAR_VARS)
-
-      LOCAL_PREBUILT_EXECUTABLES := \
-        bin/$(TARGET_ARCH)/sample_decode \
-        bin/$(TARGET_ARCH)/sample_encode \
-        bin/$(TARGET_ARCH)/mfx_player \
-        bin/$(TARGET_ARCH)/mfx_transcoder
-
-      LOCAL_MODULE_TAGS := optional
-      include $(BUILD_MULTI_PREBUILT)
-    endif
-  endif
 
 endif
