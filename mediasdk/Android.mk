56b8d6094416b598c3a7f0b5a82b3bc3fa9014f7
TianmiChen <tianmi.chen@intel.com>
2018-07-12 16:38:14 +0800

    [celadon] Add mediasdk omx plugin support
    Signed-off-by: TianmiChen <tianmi.chen@intel.com>

---
+++ mediasdk/Android.mk
@@ -0,0 +1,32 @@
+LOCAL_PATH := $(call my-dir)
+
+ifneq ($(filter $(TARGET_BOARD_PLATFORM),broxton gmin joule minnowboardv3 project-celadon),)
+
+    # Shared libraries
+    include $(CLEAR_VARS)
+    LOCAL_MODULE := libmfx_omx_core
+    LOCAL_PROPRIETARY_MODULE := true
+    LOCAL_MODULE_CLASS := SHARED_LIBRARIES
+    LOCAL_MODULE_TAGS := optional
+    LOCAL_MODULE_STEM := libmfx_omx_core
+    LOCAL_MODULE_SUFFIX := $(TARGET_SHLIB_SUFFIX)
+    LOCAL_MODULE_OWNER := intel
+    LOCAL_MULTILIB := both
+    LOCAL_SRC_FILES_32 := lib/x86/libmfx_omx_core$(LOCAL_MODULE_SUFFIX)
+    LOCAL_SRC_FILES_64 := lib/x86_64/libmfx_omx_core$(LOCAL_MODULE_SUFFIX)
+    include $(BUILD_PREBUILT)
+
+    include $(CLEAR_VARS)
+    LOCAL_MODULE := libmfx_omx_components_hw
+    LOCAL_PROPRIETARY_MODULE := true
+    LOCAL_MODULE_CLASS := SHARED_LIBRARIES
+    LOCAL_MODULE_TAGS := optional
+    LOCAL_MODULE_STEM := libmfx_omx_components_hw
+    LOCAL_MODULE_SUFFIX := $(TARGET_SHLIB_SUFFIX)
+    LOCAL_MODULE_OWNER := intel
+    LOCAL_MULTILIB := both
+    LOCAL_SRC_FILES_32 := lib/x86/libmfx_omx_components_hw$(LOCAL_MODULE_SUFFIX)
+    LOCAL_SRC_FILES_64 := lib/x86_64/libmfx_omx_components_hw$(LOCAL_MODULE_SUFFIX)
+    include $(BUILD_PREBUILT)
+
+endif
