ff21d3d3824616792ae4c117adc0248a14f5e9ea
TianmiChen <tianmi.chen@intel.com>
2018-07-12 16:38:14 +0800

    [celadon] Add mediasdk omx plugin support
    Signed-off-by: TianmiChen <tianmi.chen@intel.com>

---
+++ Android.mk
@@ -0,0 +1,12 @@
+ifeq ($(USE_MEDIASDK),true)
+  LOCAL_PATH := $(call my-dir)
+  include $(CLEAR_VARS)
+
+  LOCAL_PROPRIETARY_MODULE := true
+
+  MEDIASDK_BIN_REPO := $(LOCAL_PATH)
+
+  # Call appropriate Android.mk for the platform
+  include $(MEDIASDK_BIN_REPO)/mediasdk/Android.mk
+
+endif
