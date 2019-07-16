59b4cb3aba2f0b4dbdbdaf4266f6fd7f5893efd1
tianmi.chen <tianmi.chen@intel.com>
2019-07-16 11:14:00 +0800

    Integrate Opensource MediaSDK OpenMAX Plugin to 1A
    Disable closed mediasdk openmax plugin release.
    
    Change-Id: I049b14c834c033cc580cfd817a981698f297e937
    Tracked-On: https://jira.devtools.intel.com/browse/OAM-77048
    Signed-off-by: tianmi.chen <tianmi.chen@intel.com>
    Reviewed-on: https://android.intel.com:443/675300

--- Android.mk
+++ Android.mk
@@ -1,23 +1,24 @@
 ifeq ($(USE_MEDIASDK),true)
-  LOCAL_PATH := $(call my-dir)
-  include $(CLEAR_VARS)
+  ifneq ($(BOARD_HAVE_OMX_SRC),true)
+    LOCAL_PATH := $(call my-dir)
+    include $(CLEAR_VARS)
 
-  LOCAL_COPY_HEADERS_TO := msdk/openmax
+    LOCAL_COPY_HEADERS_TO := msdk/openmax
 
-  LOCAL_COPY_HEADERS := \
-    openmax/OMX_IntelColorFormatExt.h \
-    openmax/OMX_IntelErrorTypes.h \
-    openmax/OMX_IntelIndexExt.h \
-    openmax/OMX_IntelVideoExt.h
+    LOCAL_COPY_HEADERS := \
+      openmax/OMX_IntelColorFormatExt.h \
+      openmax/OMX_IntelErrorTypes.h \
+      openmax/OMX_IntelIndexExt.h \
+      openmax/OMX_IntelVideoExt.h
 
-  LOCAL_PROPRIETARY_MODULE := true
+    LOCAL_PROPRIETARY_MODULE := true
 
-  include $(BUILD_COPY_HEADERS)
+    include $(BUILD_COPY_HEADERS)
 
-  MEDIASDK_BIN_REPO := $(LOCAL_PATH)
+    MEDIASDK_BIN_REPO := $(LOCAL_PATH)
 
-  # Call appropriate Android.mk for the platform
-  include $(MEDIASDK_BIN_REPO)/mediasdk/broxton/Android.mk
-  include $(MEDIASDK_BIN_REPO)/mediasdk/icelake/Android.mk
-
+    # Call appropriate Android.mk for the platform
+    include $(MEDIASDK_BIN_REPO)/mediasdk/broxton/Android.mk
+    include $(MEDIASDK_BIN_REPO)/mediasdk/icelake/Android.mk
+  endif
 endif
