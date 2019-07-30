7009cca341c172b8f3a46da6cc470c123381b9ab
LukeZheZhu <luke.zhezhu@gmail.com>
2019-07-30 13:47:58 +0800

    Update Android.mk for OMX Plugin open source
    	Update Android.mk in media_release to enale OMX Plugin
    	compile for source code.

--- Android.mk
+++ Android.mk
@@ -1,12 +1,13 @@
-ifeq ($(USE_MEDIASDK),true) && ($(BOARD_HAVE_OMX_SRC),false)
-  LOCAL_PATH := $(call my-dir)
-  include $(CLEAR_VARS)
+ifeq ($(USE_MEDIASDK),true)
+  ifneq ($(BOARD_HAVE_OMX_SRC),true)
+    LOCAL_PATH := $(call my-dir)
+    include $(CLEAR_VARS)
+    
+    LOCAL_PROPRIETARY_MODULE := true
 
-  LOCAL_PROPRIETARY_MODULE := true
+    MEDIASDK_BIN_REPO := $(LOCAL_PATH)
 
-  MEDIASDK_BIN_REPO := $(LOCAL_PATH)
-
-  # Call appropriate Android.mk for the platform
-  include $(MEDIASDK_BIN_REPO)/mediasdk/Android.mk
-
+    # Call appropriate Android.mk for the platform
+    include $(MEDIASDK_BIN_REPO)/mediasdk/Android.mk
+  endif
 endif
