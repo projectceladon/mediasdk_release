9f5aa8146d52cc0380c48247ab4df437773c6737
tianmi.chen <tianmi.chen@intel.com>
2019-09-05 10:41:48 +0800

    Integrate Opensource MediaSDK OpenMAX Plugin to 1A
    This change is to disable closed mediasdk openmax plugin release.
    
    Tracked-On: https://jira.devtools.intel.com/browse/OAM-76499
    Signed-off-by: tianmi.chen <tianmi.chen@intel.com>

--- Android.mk
+++ Android.mk
@@ -1,12 +1,13 @@
 ifeq ($(USE_MEDIASDK),true)
-  LOCAL_PATH := $(call my-dir)
-  include $(CLEAR_VARS)
+  ifneq ($(BOARD_HAVE_OMX_SRC), true)
+    LOCAL_PATH := $(call my-dir)
+    include $(CLEAR_VARS)
 
-  LOCAL_PROPRIETARY_MODULE := true
+    LOCAL_PROPRIETARY_MODULE := true
 
-  MEDIASDK_BIN_REPO := $(LOCAL_PATH)
+    MEDIASDK_BIN_REPO := $(LOCAL_PATH)
 
-  # Call appropriate Android.mk for the platform
-  include $(MEDIASDK_BIN_REPO)/mediasdk/Android.mk
-
+    # Call appropriate Android.mk for the platform
+    include $(MEDIASDK_BIN_REPO)/mediasdk/Android.mk
+  endif
 endif
